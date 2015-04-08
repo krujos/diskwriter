94fca99960df493f632e7c41e1e0850b6293ddb8
krujos <jkruck@pivotal.io>
2015-04-08 17:46:26 -0500

    InitialCommit

---
+++ src/main/java/diskwriter/DiskWriterApplication.java
@@ -0,0 +1,12 @@
+package diskwriter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiskWriterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiskWriterApplication.class, args);
+    }
+}
---
+++ src/test/java/diskwriter/DiskWriterApplicationTests.java
@@ -0,0 +1,16 @@
+package diskwriter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = DiskWriterApplication.class)
+public class DiskWriterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
