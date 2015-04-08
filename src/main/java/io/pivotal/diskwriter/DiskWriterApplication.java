9bb04e10cd0085258eef017d992e9c1f6b53385b
krujos <jkruck@pivotal.io>
2015-04-08 17:50:39 -0500

    Fix packages

--- src/main/java/io/pivotal/diskwriter/DiskWriterApplication.java
+++ src/main/java/io/pivotal/diskwriter/DiskWriterApplication.java
@@ -1,4 +1,4 @@
-package diskwriter;
+package io.pivotal.diskwriter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/pivotal/diskwriter/DiskWriterApplicationTests.java
+++ src/test/java/io/pivotal/diskwriter/DiskWriterApplicationTests.java
@@ -1,4 +1,4 @@
-package diskwriter;
+package io.pivotal.diskwriter;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
